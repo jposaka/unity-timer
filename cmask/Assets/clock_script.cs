7ebe084bf9a28375b4897fd14eb3ea10fb020db5
kookuzira2000 <koo3763@gmail.com>
2017-09-03 13:58:36 +0900

    unity ヌルヌル動く 簡単タイマー
    unity 2017で作成。unity 5.x～

---
+++ cmask/Assets/clock_script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//need using UI
+
+public class clock_script : MonoBehaviour {
+    public int maxtime = 20;
+
+    public Image gauge; //move circle
+    public Text time_text;
+    
+
+    void Start()
+    {
+        maxtime = 20;
+    }
+
+    public void view_up_time(int now_time, float x)
+    {if (now_time > maxtime) return;
+      
+        gauge.fillAmount = (float)x / maxtime;
+        time_text.text = now_time.ToString();
+    }
+
+    public void resetfill() {
+        gauge.fillAmount = 0;
+        time_text.text = "0";
+    }
+  
+}
---
+++ cmask/Assets/timer_script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer_script : MonoBehaviour {
+
+    public clock_script _crock_script; //script view clock
+    
+    public int  now_time;
+
+    public float timeOut=1f;  // interval time
+    private float timeElapsed; //internal interval time
+
+    void Start()
+    {
+      
+    }
+
+    
+    //when press
+    public void start_bt() {
+        now_time = 0; 
+        _crock_script.view_up_time(0,0);
+    }
+
+
+
+
+    
+    void Update()
+    {
+       
+       
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut)
+        {
+          
+          
+            ++now_time;
+           
+            timeElapsed = 0.0f;
+        }
+        _crock_script.view_up_time(now_time, (float)(now_time+timeElapsed));
+    }
+}
